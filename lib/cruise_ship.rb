d14a1cd2ee493882baaceb2bd40994c7d5d5df40
Kasey Arnold <kaseyarnold15@gmail.com>
2020-02-01 01:13:58 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,16 +6,7 @@
 #suite_e: "Crumpet the Elf"
 #}
 
-def select_winner(passengers)
-  
-  winner = ""
-  passengers.each do |suit, name|
-    if suite == :suite_a && name.start_with?("A")
-  winner = name
-  end
-end
-winner
-end
+
 
 
 # add the code snippet here!
